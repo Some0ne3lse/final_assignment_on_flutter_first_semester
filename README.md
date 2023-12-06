5d2f8bf91d2236e724d3b12571d9f2d4ea6bf69b
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2023-12-06 15:58:12 +0000

    First commit!

---
+++ README.md
@@ -0,0 +1,3 @@
+# final_assignment_on_flutter
+
+This is my attempt to convert my dart console application to a mobile app.
---
+++ android/app/src/main/kotlin/com/example/final_assignment_on_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.final_assignment_on_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
